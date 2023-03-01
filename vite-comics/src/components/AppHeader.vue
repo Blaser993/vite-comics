57bc4950bdc91154221dfbcc59270b4324ed631f
Blaser993 <corradodepinto.design@gmail.com>
2023-03-01 18:12:01 +0100

    FEAT Style added in AppHeader.vue

--- vite-comics/src/components/AppHeader.vue
+++ vite-comics/src/components/AppHeader.vue
@@ -1,19 +1,19 @@
 <template>
     <header>
         <div class="container">
-            <img src="/images/dc-logo.png" alt="">
-            <nav>
-                <ul class="main-menu">
-                    <li class="menu-item active">link</li>
-                    <li class="menu-item">link</li>
-                    <li class="menu-item">link</li>
-                    <li class="menu-item">link</li>
-                    <li class="menu-item">link</li>
-                    <li class="menu-item">link</li>
-                    <li class="menu-item">link</li>
-                    <li class="menu-item">link</li>
-                    <li class="menu-item">link</li>
-                    <li class="menu-item">link</li>
+            <nav class="row">
+                <img class="col-auto" src="/img/dc-logo.png" alt="">
+                <ul class="col-auto main-menu">
+                    <li class="menu-item active">characters</li>
+                    <li class="menu-item">comics</li>
+                    <li class="menu-item">movies</li>
+                    <li class="menu-item">tv</li>
+                    <li class="menu-item">games</li>
+                    <li class="menu-item">collectibles</li>
+                    <li class="menu-item">videos</li>
+                    <li class="menu-item">fans</li>
+                    <li class="menu-item">news</li>
+                    <li class="menu-item">shop</li>
                 </ul>
             </nav>
         </div>
@@ -28,4 +28,27 @@
 
 <style lang="scss" scoped>
 
+    nav{
+        align-items: center;
+        justify-content: space-between;
+    }
+    .main-menu{
+        display: flex;
+        justify-content: flex-end;
+    }
+    .menu-item{
+        margin: 0 1rem;
+        line-height: 108px;
+        text-transform: uppercase;
+        font-weight: 700;
+
+        &.active{
+            color: cornflowerblue;
+            border-bottom: 4px solid cornflowerblue;
+        }
+    }
+
+    img{
+        max-width: 80px;
+    }
 </style>
