adc04d35759c8d8ced91093e5a2843cffedb6aff
Blaser993 <corradodepinto.design@gmail.com>
2023-03-02 16:11:03 +0100

    FEAT added AppMain.vue basic layout

--- vite-comics/src/components/AppMain.vue
+++ vite-comics/src/components/AppMain.vue
@@ -1,7 +1,22 @@
 <template>
-    <main class="content row">
-        <div class="container">
-        __> content goes here  __
+    <main class="content">
+        <div class="container-full">
+
+            <div class="container">
+                <div class="row">
+                    <div class="col-auto">
+                        <h2>current series</h2>
+                    </div>
+                </div>
+                <div class="row">
+                    comics
+                </div>
+                <div class="row">
+                    <col-auto><button>load now</button></col-auto>
+                </div>
+
+            </div>
+
         </div>
     </main>
 </template>
@@ -14,13 +29,20 @@
 
 <style lang="scss" scoped>
     .content{
-        align-items: center;
+        -align-items: center;
         min-height: 100px;
         background-color: black;
         color: white;
+        background-image:url("/img/jumbotron.jpg");
+        background-size: cover;
+        padding-top: 20rem;
     }
 
-    .container{
-        text-align: start;
+
+ 
+
+    .container-full{
+        width: 100%;
+        background-color: rgb(48, 48, 48);
     }
 </style>
