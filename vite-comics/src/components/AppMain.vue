b539c8ecd93e657c4a695b85114d4dbe1d64814d
Blaser993 <corradodepinto.design@gmail.com>
2023-03-02 16:44:56 +0100

    FEAT basic content added in AppMain.vue

--- vite-comics/src/components/AppMain.vue
+++ vite-comics/src/components/AppMain.vue
@@ -4,15 +4,32 @@
 
             <div class="container">
                 <div class="row">
-                    <div class="col-auto">
+                    <div class="col-auto title">
                         <h2>current series</h2>
                     </div>
                 </div>
+
                 <div class="row">
-                    comics
+                    <div class="col-auto card">
+                        
+
+                        <img class="card-image" src="/img/screen-7.jpg" alt="pic">
+                        
+                        <h5>titolo</h5>
+                       
+                            
+                        
+                    </div>
+
+                    <div class="col-auto">
+                        <div class="card">ciao</div>
+                    </div>
                 </div>
+
                 <div class="row">
-                    <col-auto><button>load now</button></col-auto>
+                    <col-auto class="cta">
+                        <button>load now</button>
+                    </col-auto>
                 </div>
 
             </div>
@@ -29,7 +46,7 @@
 
 <style lang="scss" scoped>
     .content{
-        -align-items: center;
+        align-items: center;
         min-height: 100px;
         background-color: black;
         color: white;
@@ -38,11 +55,45 @@
         padding-top: 20rem;
     }
 
+    .title{
+        font-size: 20px;
+        text-transform: uppercase;
+        background-color: cornflowerblue;
+        max-width: fit-content;
+        padding: 0.5rem 2.5rem;
+        margin-top: -2rem;
+        margin-bottom: 1rem;
+    }
+    
+
+    .card {
+        padding: 1em;
+        
+    }
+
+    
+   
+
+    .card-image{
+        max-width: 20%;
+        aspect-ratio: 1;
+        object-fit: cover;
+    }
+
 
  
 
     .container-full{
         width: 100%;
         background-color: rgb(48, 48, 48);
+    }
+
+
+    .cta{
+        text-align: center;
+        align-items: center;
+        align-content: center;
+        align-self: center;
+        justify-self: center;
     }
 </style>
